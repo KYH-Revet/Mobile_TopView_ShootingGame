2455222c3705911112b6e1790179fd95c54d94f4
Revet <75615511+KYH-Revet@users.noreply.github.com>
2024-09-28 21:57:15 +0900

    Add Feature : Letter Box
    Letter Box(9:16)

--- Assets/Scripts/Systems/CamFollowPlayer.cs
+++ Assets/Scripts/Systems/CamFollowPlayer.cs
@@ -17,13 +17,20 @@ public class CamFollowPlayer : MonoBehaviour
     void Start()
     {
         target = Player.instance.transform;
+
+        LetterBox();
     }
 
+    
     // Update is called once per frame
     void Update()
     {
         Follow();
     }
+
+    // Outside from letter box
+    void OnPreCull() => GL.Clear(true, true, Color.black);
+
 
     void Follow()
     {
@@ -32,4 +39,26 @@ public class CamFollowPlayer : MonoBehaviour
         if(position.z > max)    position.z = max;
         transform.position = position;
     }
+
+    void LetterBox()
+    {
+        Camera camera = GetComponent<Camera>();
+        Rect rect = camera.rect;
+        float scaleheight = ((float)Screen.width / Screen.height) / (9f / 16f); // (가로 / 세로)
+        float scalewidth = 1f / scaleheight;
+        
+        if (scaleheight < 1)    // 
+        {
+            rect.height = scaleheight;
+            rect.y = (1f - scaleheight) / 2f;
+        }
+        else                    // 
+        {
+            rect.width = scalewidth;
+            rect.x = (1f - scalewidth) / 2f;
+        }
+        camera.rect = rect;
+    }
+
+    
 }
